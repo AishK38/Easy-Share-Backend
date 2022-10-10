149ab71f45ee5540a48eea20d91f6b23351f4c75
Aishwarya Karmakar <80018881+AishK38@users.noreply.github.com>
2022-10-10 13:59:58 +0530

    added gitignore and db files

--- config/dbase.js
+++ config/dbase.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
+require('dotenv/config')
 const mongoose = require('mongoose');
+
 
 function connectDB() {
     // DB connection
-    mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true});
-    const connection = mongoose.connection;
-
-    connection.once('open', () => {
-        console.log('Database connected.');
-    }).catch(err => {
-        console.log('Connection failed.');
-    })
+    mongoose.connect(process.env.MONGO_URL,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+ })
+ .then(() => console.log("DB is connected"))
+ .catch((err) => console.log(err));
 } 
+
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const connectDB = require('./config/dbase');
+connectDB();
+
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`);
-})
+});
