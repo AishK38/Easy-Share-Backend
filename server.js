7e33b5c66c1871a254a9b65c7c471edc9a63db4c
Aishwarya Karmakar <80018881+AishK38@users.noreply.github.com>
2022-10-25 10:45:10 +0530

    added cors and style

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const cors = require('cors');
 app.use(express.static('public'));
+
 
 const connectDB = require('./config/dbase');
 connectDB();
 
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
